43cbe1e53ea3b9a3fb2ff30fadb7ed5c1854bc95
Kosuke2000 <youuchiharu@gmail.com>
2022-03-30 07:16:48 +0000

    feat: Fakerを使ってダミーデータを生成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\Info::factory()->count(10)->create();
     }
 }
---
+++ database/seeders/InfoTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class InfoTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('info')->insert([
+            'id' => 5,
+            'description' => Str::random(10),
+        ]);
+    }
+}
